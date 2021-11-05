8d117ae654af860c27e6912a0afb8d1b7e2fc617
“Mikel” <goikoetxeamikel85@gmail.com>
2021-11-05 11:04:55 +0100

    Colaborador: Mikel Añadido clase Baile y modificado el index

--- clases/NsDanza.php
+++ clases/NsDanza.php
@@ -54,5 +54,42 @@ class Actividad
     }
 }
 
+class Baile extends Actividad
+{
+    static $profesor;
+    private $claseHora;
 
-?>
+    function __construct($id, $descripcion, $fecha, $lugar, $profesor, $claseHora)
+    {
+        parent::__construct($id, $descripcion, $fecha, $lugar);
+        Baile::$profesor = $profesor;
+        $this->claseHora = $claseHora;
+    }
+
+    public function getProfesor() 
+    {
+        return $this->profesor;
+    }
+
+    public function setProfesor($profesor)
+    {
+        Baile::$profesor = $profesor;
+    }
+    public function getClaseHora() 
+    {
+        return $this->claseHora;
+    }
+
+    public function setClaseHora($claseHora)
+    {
+        $this->claseHora = $claseHora;
+    }
+
+    function mostrar()
+    {
+        $mensaje = parent::mostrar();
+        $mensaje .=" y la realizara ".Baile::$profesor." que cobra la hora ".$this->claseHora."€";
+        return $mensaje;
+
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
     include "helper/funciones.php";
     include "clases/NsDanza.php";
+    $act = new Actividad(1, "Cumpleaños de Mikel", '16-01-2022', "Baluarte");
+    $bai = new Baile(2, "Bailes de Salón", '01-01-2022', "Teatro de Pamplona", "Asier Gonzalez", 35);
+    $resultado = "";
+
+    $resultado .= $act->mostrar()."<br>";
+    $act->setDescripcion("Evento benéfico");
+    $act->setFecha('11-09-2021');
+    $resultado .= $act->mostrar()."<br>";
+    $resultado .= $bai->mostrar()."<br>";
+    $bai->setProfesor("Pedro Pedrosa");
+    $bai->setLugar("Teatro Lara");
+    $bai->setClaseHora(50);
+    $resultado .= $bai->mostrar();
+
+
 
     include "views/vista_resultado.php";
-?>
